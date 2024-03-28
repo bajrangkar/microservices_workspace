02e3947a8d9c8d215280ad176bb853c74bd92d2c
BumbleBee <bajrangkar@outlook.com>
2024-03-28 16:33:35 +0530

    JavaTechOnline - Fault tolerance - RateLimiter, Retry

---
+++ eureka/fault-check/src/main/java/com/myfirst/microservices/faultcheck/FaultCheckApplication.java
@@ -0,0 +1,13 @@
+package com.myfirst.microservices.faultcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaultCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaultCheckApplication.class, args);
+    }
+
+}
